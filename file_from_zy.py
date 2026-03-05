3aadf90b2a6d15e15cd251d4b16accbab7e993e7
zhenyao <zhenyao@ZhenyaoMacBook-Air.local>
2026-03-06 01:03:42 +0800

    feat: add favmovie function

--- file_from_zy.py
+++ file_from_zy.py
@@ -2,3 +2,8 @@ def hello(x):
     print("Hi," + x + "!")
 x = input("Enter your name: ")
 hello(x)
+
+def favmovie(movie):
+    print("Your favorite movie is " + movie + ".")
+movie = input("What is your favorite movie? ")
+favmovie(movie)
